359ae9955a3db1c76955be7f31b598314c4dcf1e
Ingeborg Eldøy <ieldoy@gmail.com>
2021-08-28 22:36:41 +0200

    Adjusted div img, margin/padding

--- app/pages/index.js
+++ app/pages/index.js
@@ -7,46 +7,43 @@ module.exports = async function($) {
         text-align: left;
         background-color: hsla(200,100%,25%,0.1);
         font-family: monospace;
-        padding-top: 3rem;
+        padding-top: 2.5rem;
       }
 
-      h1 {
-        text-align: center;
-        font-size: 20px;
-        margin: 20px;
+      h3,
+      h2 {
+        font-size: 25px;
+        margin: 10px;
       }
 
-      img {
-        margin-top: 90px;
-        margin-bottom: 35px;
-        margin-right: 25px;
-        margin-left: 5px;
-        position: relative;
+      #img {
+        display: flex; justify-content: center;
       }
 
       p {
 
-        margin: 25px;
+        margin: 5px 20px 20px 15px;
         text-align: left;
-
+        font-size: 16px;
       }
+
     </style>
-
-<img src="img/ielody-modified.png" width="150" align="right" alt="portfolio">
-
+<div id="img">
+<img src="img/ielody-modified.png" width="160" align=middle alt="portfolio">
+</div>
 <h2>About me</h2>
-<p>
-After years of traveling from continent to continent I have settled amongst the tallest people in the world, in Amsterdam
-right by the canal houses. I might be a datamapper for the media company Nielsen for now, but my ambition is to work as
-an expert coder and designer. My education spans over a lot of topics, mainly computer science, languages and media science.
+
+<p>After years of traveling and living I have settled amongst the canal houses in Amsterdam.
+I might be a datamapper for the media company Nielsen for now, but my ambition is to work as
+a web developer and designer. My education covers a lot of topics; computer science, languages and media science.
 </p>
+
 <hr>
 <h3>In the works</h3>
 <p>
-      Currently I am studying to become a front end developer to build functional websites that are easy on the eye,
-      in close co-operation with the client. After all, isn't that what we all want? My portfolio is constantly expanding,
-      and will be available on this page as soon as they are finished.
-    </p>
+      I build functional websites that are easy on the eye and user-friendly, and believe that a clean and simple design
+      is usually the best. In addition to coding projects I also do some writing, both of which you can find in My portfolio.
+      </p>
 
   `
 }
