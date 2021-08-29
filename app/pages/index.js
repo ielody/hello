f0347ef30f2cca8b551ef2db5c6962459a21dfb0
Ingeborg Eldøy <ieldoy@gmail.com>
2021-08-29 19:07:33 +0200

    edited text, made list, email

--- app/pages/index.js
+++ app/pages/index.js
@@ -18,32 +18,49 @@ module.exports = async function($) {
 
       #img {
         display: flex; justify-content: center;
+        margin: 20px;
       }
 
       p {
 
-        margin: 5px 20px 20px 15px;
+        margin: 5px 20px 25px 25px;
         text-align: left;
         font-size: 16px;
       }
 
+      #banner {
+      position: top;
+      }
+
     </style>
+
+<div id="banner"></div>
+
 <div id="img">
 <img src="img/ielody-modified.png" width="160" align=middle alt="portfolio">
 </div>
+
+
+
 <h2>About me</h2>
 
-<p>After years of traveling and living I have settled amongst the canal houses in Amsterdam.
-I might be a datamapper for the media company Nielsen for now, but my ambition is to work as
-a web developer and designer. My education covers a lot of topics; computer science, languages and media science.
+<p>
+Hello! My name is Ingeborg and I'm a problem-solver. My love of
+new challenges has brought me on the path of <strong>web development</strong>, and
+I build user-friendly websites with a clean and simple design. No fuzz. No drama.<br><br>
+I also have experience as a <strong>SEO writer</strong> and worked as a freelance Norwegian <strong>translator</strong> for years.
+Take a look at my portfolio to see examples of what I can do.
 </p>
 
 <hr>
-<h3>In the works</h3>
-<p>
-      I build functional websites that are easy on the eye and user-friendly, and believe that a clean and simple design
-      is usually the best. In addition to coding projects I also do some writing, both of which you can find in My portfolio.
-      </p>
+<h3>What I can help you with</h3>
+<ul>
+      <li>Build you a simple and user-friendly website</li>
+      <li>Write blog articles with SEO for your website</li>
+      <li>Be your personal cheerleader (although I'm not really qualified for that)</li>
 
+</ul>
+<footer>
+<a href="mailto:ieldoy@gmail.com">Send me an email if you're curious</a></footer>
   `
 }
